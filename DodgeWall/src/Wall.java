1ecf209fcf66309dea6b1d119f43ae7fa6d50a34
hirany <devhirany@gmail.com>
2019-11-05 16:12:10 +0900

    BackgroundとWallクラスのプロパティとメソッドを定義した_#4

--- DodgeWall/src/Wall.java
+++ DodgeWall/src/Wall.java
@@ -1,4 +1,23 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
 
-public class Wall {
+import javax.swing.JPanel;
+
+public class Wall extends JPanel {
+
+	private int height = 1;
+	private Color color = new Color(255, 0, 255, 0);
+	private Point points[] = new Point[4];
+	private String name = new String();
+
+	public void Wall(int displayHeight, String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+	}
+
 
 }
