c06612ddd8ed0930c6803f2e4d36e4a4a0417394
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-11-12 21:34:14 +0900

    Mainクラスを仮実装 #18

--- DodgeWall/src/Main.java
+++ DodgeWall/src/Main.java
@@ -14,8 +14,8 @@ public class Main {
 			frame.setVisible(true);
 			frame.setSize(500, 1000);
 			frame.setLocationRelativeTo(null);
-	        frame.setFocusable(true);
-	        frame.setResizable(false);
+			frame.setFocusable(true);
+			frame.setResizable(false);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		});
 	}
