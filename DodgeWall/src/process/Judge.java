40d3d1e193550c1cab7f96bc509330314f07f209
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-11-12 20:12:30 +0900

    Judge.javaを仮実装 #8

--- DodgeWall/src/process/Judge.java
+++ DodgeWall/src/process/Judge.java
@@ -2,7 +2,16 @@ package process;
 import java.awt.*;
 
 public class Judge {
-    public static boolean hitJudge (Point ballPoint, int ballSize, Point wallPoint[], int wallSpeed) {
+    public static boolean hitJudge (Point ballPoint, int ballSize, Polygon wallPoint, int wallSpeed) {
+        if ( ballPoint.x != wallPoint.xpoints[3] ) { // wallPointの4番目に壁の左下隅の値が入っているものとして考えている
+            return false;
+        }
+        if ( ballSize > wallSpeed && ( ballPoint.y > wallPoint.ypoints[3] || ballPoint.y + 2 * ballSize < wallPoint.ypoints[3] ) ) {
+            return false;
+        }
+        if ( ballSize < wallSpeed && ( ballPoint.y > wallPoint.ypoints[3] || ballPoint.y + 2 * ballSize < wallPoint.ypoints[3] - wallSpeed ) ) {
+            return false;
+        }
         return true;
     }
 }
