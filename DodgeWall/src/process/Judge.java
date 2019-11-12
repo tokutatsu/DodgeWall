374f663720f1fb1de43cdb1b5512f50044c7bfd0
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-11-12 20:24:59 +0900

    ballSizeとwallSpeedに関してコメントを追加 #8

--- DodgeWall/src/process/Judge.java
+++ DodgeWall/src/process/Judge.java
@@ -2,7 +2,7 @@ package process;
 import java.awt.*;
 
 public class Judge {
-    public static boolean hitJudge (Point ballPoint, int ballSize, Polygon wallPoint, int wallSpeed) {
+    public static boolean hitJudge (Point ballPoint, int ballSize, Polygon wallPoint, int wallSpeed) { // ballSizeとwallSpeedは今後configから持ってくる
         if ( ballPoint.x != wallPoint.xpoints[3] ) { // wallPointの4番目に壁の左下隅の値が入っているものとして考えている
             return false;
         }
