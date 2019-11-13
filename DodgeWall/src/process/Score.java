e8575d551a72be14acd4efad8ffb56c46ba5be5c
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-11-14 02:19:56 +0900

    トップ10のスコアを返すメソッドの実装 #30

--- DodgeWall/src/process/Score.java
+++ DodgeWall/src/process/Score.java
@@ -7,7 +7,28 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Score {
-	public static int getMyHighScore() throws Exception {
+	public static int[] getTopTenScore() throws Exception { // 全ての記録の中でトップ10を返す
+		try (
+				Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/DodgeWall",
+						"postgres",
+						"postgres");
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(
+						"select score from scores order by score desc offset 0 limit 10"
+						);
+				){
+			int[] score = new int[resultSet.getRow()];
+			for ( int i = 0; i < resultSet.getRow(); i++ ) {
+				score[i] = resultSet.getInt(i);
+			}
+			return score;
+		} catch ( SQLException e ) {
+			int[] score = {-1};
+			return score;
+		}
+	}
+
+	public static int getMyHighScore() throws Exception { //すべての記録の中で一番高いスコアを返す
 		try (
 				Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/DodgeWall",
 						"postgres",
@@ -24,7 +45,7 @@ public class Score {
 		}
 	}
 
-	public static boolean setMyHighScore(int score) throws Exception {
+	public static boolean setMyHighScore(int score) throws Exception { //名無しのプレイヤーのプレイ記録を記録する
 		try (
 				Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/DodgeWall",
 						"postgres",
