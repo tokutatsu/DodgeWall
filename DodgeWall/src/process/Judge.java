30769546255b1acdb4f9a1695f51c96cc08e9579
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-11-15 18:54:24 +0900

    Judge.javaの座標を整理 #34

--- DodgeWall/src/process/Judge.java
+++ DodgeWall/src/process/Judge.java
@@ -1,21 +1,20 @@
 package process;
-import java.awt.Point;
-import java.awt.Polygon;
-
 import config.BallConfig;
 import config.WallConfig;
+import unit.Ball;
+import unit.Wall;
 
 public class Judge {
-    public static boolean hitJudge (Point ballPoint, Polygon wallPoint) {
+    public static boolean hitJudge (Ball ball, Wall wall) {
     	int ballSize = BallConfig.size;
     	int wallSpeed = WallConfig.yBottomIncrease;
-        if ( ballPoint.x != wallPoint.xpoints[WallConfig.buttomLeft] ) { // wallPointの3番目に壁の左下隅の値が入っていました
+        if ( ! wall.getName().endsWith(Integer.toString(ball.getPosition())) ) {
             return false;
         }
-        if ( ballSize > wallSpeed && ( ballPoint.y > wallPoint.ypoints[WallConfig.buttomLeft] || ballPoint.y + 2 * ballSize < wallPoint.ypoints[WallConfig.buttomLeft] ) ) {
+        if ( ballSize > wallSpeed && ( ball.getPoint().y + ballSize / 2 > wall.getButtomLeft() || ball.getPoint().y + ballSize < wall.getButtomLeft() ) ) {
             return false;
         }
-        if ( ballSize < wallSpeed && ( ballPoint.y > wallPoint.ypoints[WallConfig.buttomLeft] || ballPoint.y + 2 * ballSize < wallPoint.ypoints[WallConfig.buttomLeft] - wallSpeed ) ) {
+        if ( ballSize < wallSpeed && ( ball.getPoint().y + ballSize / 2 > wall.getButtomLeft() || ball.getPoint().y + ballSize < wall.getButtomLeft() - wallSpeed ) ) {
             return false;
         }
         return true;
