06e2dd983af39e5df6e51f33e2bfebb74b8cee57
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-11-16 00:37:26 +0900

    positionを追加した #49

--- DodgeWall/src/process/Judge.java
+++ DodgeWall/src/process/Judge.java
@@ -21,18 +21,18 @@ public class Judge {
 		return false;
 	}
 
-    private static boolean checkEachWall(Ball ball, Wall wall) {
-    	int ballSize = BallConfig.size;
-    	int wallSpeed = WallConfig.yBottomIncrease;
-        if ( ! wall.getName().endsWith(Integer.toString(ball.getPosition())) ) {
-            return false;
-        }
-        if ( ballSize > wallSpeed && ( ball.getPoint().y + ballSize / 2 > wall.getButtomLeft() || ball.getPoint().y + ballSize < wall.getButtomLeft() ) ) {
-            return false;
-        }
-        if ( ballSize < wallSpeed && ( ball.getPoint().y + ballSize / 2 > wall.getButtomLeft() || ball.getPoint().y + ballSize < wall.getButtomLeft() - wallSpeed ) ) {
-            return false;
-        }
-        return true;
-    }
+	private static boolean checkEachWall(Ball ball, Wall wall) {
+		int ballSize = BallConfig.size;
+		int wallSpeed = WallConfig.yBottomIncrease;
+		if ( wall.getPosition() != ball.getPosition() ) {
+			return false;
+		}
+		if ( ballSize > wallSpeed && ( ball.getPoint().y + ballSize / 2 > wall.getButtomLeft() || ball.getPoint().y + ballSize < wall.getButtomLeft() ) ) {
+			return false;
+		}
+		if ( ballSize < wallSpeed && ( ball.getPoint().y + ballSize / 2 > wall.getButtomLeft() || ball.getPoint().y + ballSize < wall.getButtomLeft() - wallSpeed ) ) {
+			return false;
+		}
+		return true;
+	}
 }
