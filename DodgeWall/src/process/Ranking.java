a3c54db73243cc22dd692270258426d8ff7f1d91
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-11-20 21:19:56 +0900

    組込DBを使えるようにした #109

--- DodgeWall/src/process/Ranking.java
+++ DodgeWall/src/process/Ranking.java
@@ -3,17 +3,22 @@ package process;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Ranking {
 	public static ArrayList<HashMap<String, String>> getTopTenRanking() throws Exception { // 全ての記録の中でトップ10を返す
-		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dodge_wall","postgres","postgres");
-		// データベースのエラー用
-//		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dd","postgres","postgres");
+		Connection connection = DriverManager.getConnection("jdbc:h2:./db/dodge_wall","sa","");
 		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery("select name, score from ranking order by score desc, id desc offset 0 limit 10");
+		statement.executeUpdate("create table if not exists ranking (id serial primary key, name text, score int)");
+		ResultSet resultSet;
+		try {
+			resultSet = statement.executeQuery("select name, score from ranking order by score desc, id desc limit 10 offset 0");
+		} catch ( SQLException e) {
+			resultSet = statement.executeQuery("select name, score from ranking order by score desc, id desc");
+		}
 		ArrayList<HashMap<String, String>> recordList = new ArrayList<HashMap<String, String>>();
 		HashMap<String, String> record;
 		while ( resultSet.next() ) {
@@ -22,15 +27,16 @@ public class Ranking {
 			record.put("score", Integer.toString(resultSet.getInt("score")));
 			recordList.add(record);
 		}
+		connection.close();
 		return recordList;
 	}
 
-	public static void updateRanking(String userName, int score) throws Exception { // プレイ記録を記録する
-		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/dodge_wall",
-				"postgres",
-				"postgres");
+	public static void updateRanking(String userName, int score) throws Exception {
+		Connection connection = DriverManager.getConnection("jdbc:h2:./db/dodge_wall", "sa", "");
 		Statement statement = connection.createStatement();
+		statement.executeUpdate("create table if not exists ranking (id serial primary key, name text, score int)");
 		statement.executeUpdate("insert into ranking (name, score) values ('" + userName + "', " + score + ")");
+		connection.close();
 	}
 
 }
